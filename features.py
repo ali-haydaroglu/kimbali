a3e0c5423433247405ea63c1202b5939e619b6e2
Ali Haydaroglu <ali.haydaroglu@mail.utoronto.ca>
2018-09-21 18:43:40 -0400

    compute overcharrge and fees

--- features.py
+++ features.py
@@ -4,4 +4,17 @@ def compute_cases(claims):
     for provider in split_by_provider.keys():
         num_cases[provider]=len(split_by_provider[provider])
     return num_cases
-    
+
+
+def compute_overcharge_and_fees(claims):
+    average_proc_price = data.average(claims, 'procedure', 'amount')
+    split_by_provider = data.split_by(claims, 'provider_id')
+    oc = {}
+    fees = {}
+    for provider in split_by_provider.keys():
+        fees[provider] = split_by_provider[provider]['amount'].sum()
+        average_price = 0
+        for case in split_by_provider[provider]:
+            average_price += average_proc_price[case['procedure']]['avg']
+        oc[provider] = (fees[provider] - average_price)*1.0/average_price
+    return oc, fees
